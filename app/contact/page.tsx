cd69f0506e123352ebb2838369eabc13d36bfcc3
Bonga <a7mad.abu.elnaga@gmail.com>
2023-10-07 17:57:18 +0300

    change work to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Work() {
   return (
     <div className="work pages">
       <h2>under conestruction</h2>
-      <h1>this is work section</h1>
+      <h1>this is Contact section</h1>
     </div>
   )
 }
