6402ea3cd893d2753ac50aee2b609ef686ab0f2b
Bonga <a7mad.abu.elnaga@gmail.com>
2023-10-07 13:10:59 +0300

    v3- first update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function About() {
   return (
-    <div>
+    <div className="about pages">
       <h2>under conestruction</h2>
       <h1>this is about section</h1>
     </div>
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Work() {
   return (
-    <div>
+    <div className="work pages">
       <h2>under conestruction</h2>
       <h1>this is work section</h1>
     </div>
